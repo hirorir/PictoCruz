392a2184cd92d81bde1ad4328728cd3901320369
Eric Anderson <hirorir@gmail.com>
2014-04-05 14:15:41 -0700

    Distance for lat-lon equation added
    Utility function for lat-longitude distance added. Distance is in
    Kilometers.

--- Assets/Locationpls.cs
+++ Assets/Locationpls.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class Locationpls : MonoBehaviour {
 	LocationInfo li;
+
 	// Use this for initialization
 	void Start () {
 		StartCoroutine(updateLocation());
@@ -39,7 +40,23 @@ public class Locationpls : MonoBehaviour {
 
 		yield return new WaitForSeconds(0);
 	}
+
+	float getDistanceFromLatLonInKm(float lat1,float lon1,float lat2,float lon2) {
+		float R = 6371; // Radius of the earth in km
+		float dLat = deg2rad(lat2-lat1);  // deg2rad below
+		float dLon = deg2rad(lon2-lon1); 
+		float a = 
+			Mathf.Sin(dLat/2) * Mathf.Sin(dLat/2) +
+				Mathf.Cos(deg2rad(lat1)) * Mathf.Cos(deg2rad(lat2)) * 
+				Mathf.Sin(dLon/2) * Mathf.Sin(dLon/2)
+				; 
+		float c = 2 * Mathf.Atan2(Mathf.Sqrt(a), Mathf.Sqrt(1-a)); 
+		float d = R * c; // Distance in km
+		return d;
+	}
 	
-	
+	float deg2rad(float deg) {
+		return ((float) deg * (Mathf.PI / 180));
+	}
 	
 }
