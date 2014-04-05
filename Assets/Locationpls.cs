4572f75cb9072dc60e234482002f4c159b468bae
Eric Anderson <hirorir@gmail.com>
2014-04-05 13:28:09 -0700

    working android stuffs. GPS location in android binary.

---
+++ Assets/Locationpls.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Locationpls : MonoBehaviour {
+	float lat, lon; 
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	void Update () {
+		Input.location.Start ();
+		
+		LocationInfo li = Input.location.lastData;
+			
+		gameObject.GetComponent<GUIText> ().text = "LAT: " + li.latitude.ToString () + " LON: " + li.longitude.ToString ();
+	}
+	
+	
+	
+}
