9b07497c77f31db04f1fd729811a4ac7d3150529
Eric Anderson <hirorir@gmail.com>
2014-04-05 13:57:36 -0700

    Working Android :D
    Updated location code

--- Assets/Locationpls.cs
+++ Assets/Locationpls.cs
@@ -2,18 +2,42 @@
 using System.Collections;
 
 public class Locationpls : MonoBehaviour {
-	float lat, lon; 
+	LocationInfo li;
 	// Use this for initialization
 	void Start () {
-
+		StartCoroutine(updateLocation());
 	}
 
 	void Update () {
-		Input.location.Start ();
-		
-		LocationInfo li = Input.location.lastData;
-			
-		gameObject.GetComponent<GUIText> ().text = "LAT: " + li.latitude.ToString () + " LON: " + li.longitude.ToString ();
+
+	}
+
+	IEnumerator updateLocation(){
+		gameObject.GetComponent<GUIText>().text = "Updating Location...";
+		if (!Input.location.isEnabledByUser)
+			gameObject.GetComponent<GUIText>().text = "Location not supported on this platform";
+			yield return new WaitForSeconds(0);
+		Input.location.Start();
+		int maxWait = 10;
+		while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0) {
+			gameObject.GetComponent<GUIText>().text = "Waiting " + maxWait + " seconds";
+			yield return new WaitForSeconds(1);
+			maxWait--;
+		}
+		if (maxWait < 1) {
+			gameObject.GetComponent<GUIText>().text = "Timed out";
+			yield return new WaitForSeconds(0);
+		}
+		if (Input.location.status == LocationServiceStatus.Failed) {
+			gameObject.GetComponent<GUIText>().text = "Unable to determine device location";
+			yield return new WaitForSeconds(0);
+		} else
+			li = Input.location.lastData;
+		Input.location.Stop();
+
+		gameObject.GetComponent<GUIText>().text = "LAT: " + li.latitude + " LON: " + li.longitude + " ALT: " + li.altitude;
+
+		yield return new WaitForSeconds(0);
 	}
 	
 	
